cd7b39c1e15da3f33cb40d399a2d04142f596e47
MhammadAljawhari <mohamd.aljawhari@gmail.com>
2024-09-13 18:30:00 +0300

    nothing

--- php/form.php
+++ php/form.php
@@ -8,7 +8,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
-    <form action="../index.php" id="note-form" method="post">
+    <form action="../admin.php" id="note-form" method="post">
         <h2>Enter Note Information</h2>
 
         <!-- Report Title -->
